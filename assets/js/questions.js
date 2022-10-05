3d869d8f100f0b85126b4da90b06bc7ebf48f833
livmeierx <liv.meier13@gmail.com>
2022-10-05 19:26:10 -0400

    added elements and buttons

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,28 @@
+var containerQuestionEl = document.getElementById("question-container");
+var containerStartEl = document.getElementById("starter-container");
+var containerEndEl = document.getElementById("end-container");
+var containerScoreEl = document.getElementById("score-banner");
+var formInitials = document.getElementById("intials-form");
+var containerHighScoreEl = document.getElementById("high-score-container");
+var ViewHighScoreEl = document.getElementById("view-high-scores");
+var listHighScoreEl = document.getElementById("high-score-list");
+var correctEl = document.getElementById("correct");
+var wrongEl = document.getElementById("wrong");
+
+// buttons
+var btnStartEl = document.querySelector("#start-game");
+var btnGoBackEl = document.querySelector("#go-back");
+var btnClearScoresEl = document.querySelector("#clear-high-scores");
+
+// questions and answers
+var questionEl = document.getElementById("question");
+var answerbuttonsEl = document.getElementById("answer-buttons");
+var timeEl = document.querySelector("#timer");
+var score = 0;
+var timeleft;
+var gameover
+timerEl.innerText = 0;
+
 var questions = [
     {
       question: "Commonly used data types DO NOT include:",
